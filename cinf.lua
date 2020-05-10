dacbb16ffc796454cb53eab39ca934cc87534e59
Kherty <defendsaltair@gmail.com>
2020-05-10 18:34:53 +0200

    Update cinf.lua

--- cinf.lua
+++ cinf.lua
@@ -3,17 +3,16 @@ local computer = require("computer")
 local up = computer.uptime()
 local mem = computer.freeMemory()
 
-while true:
+while true do
 	local conditions = true
-	
-	if answer == "1":
-		print("Printing free memory and uptime...")
-		local kbmem = mem/1024
-		print("The computer has been up since", up, "seconds")
-		print("Free Memory :", kbmem)
-	elseif answer == "2":
-		print("Breaking...")
-		break
-	else
-		print("Invalid operation")
-	end
+			if answer == "1" then
+				print("Printing free memory and uptime...")
+				local kbmem = mem/1024
+				print("The computer has been up since", up, "seconds")
+				print("Free Memory :", kbmem)
+			elseif answer == "2" then 
+				print("Breaking...")
+				break
+			else then
+				print("Invalid operation")
+			end
